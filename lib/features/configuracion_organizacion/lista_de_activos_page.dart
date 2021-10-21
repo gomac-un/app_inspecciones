c56218c9c0228f90bd1e8888c130fe773b246679
victorcatn <victorcatn@users.noreply.github.com>
2021-10-20 22:52:52 -0500

    agregada la UI para las etiquetas de los activos

--- lib/features/configuracion_organizacion/lista_de_activos_page.dart
+++ lib/features/configuracion_organizacion/lista_de_activos_page.dart
@@ -1,18 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
-class ListaDeActivosPage extends StatelessWidget {
+class ActivoController {
+  final _tagsActivo = {"modelo:kenworth"};
+  late final tagsControl = fb.control<Set<String>>(_tagsActivo);
+  void guardar() => print("guardando: ${tagsControl.value}");
+  void dispose() {
+    tagsControl.dispose();
+  }
+}
+
+final _activosProvider = Provider.autoDispose((ref) => [1, 2]);
+final _activosControllersProvider = Provider.autoDispose((ref) {
+  final controllers =
+      ref.watch(_activosProvider).map((e) => ActivoController()).toList();
+  ref.onDispose(() {
+    for (var c in controllers) {
+      c.dispose();
+    }
+  });
+  return controllers;
+});
+
+class ListaDeActivosPage extends ConsumerWidget {
   const ListaDeActivosPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final controls = ref.watch(_activosControllersProvider);
     return ListView.separated(
       itemCount: 2,
       separatorBuilder: (context, index) => const Divider(),
-      itemBuilder: (context, index) => ListTile(
-        title: Hero(tag: "nombre$index", child: Text('Activo $index')),
-        subtitle: const Text('agrupamiento'),
-      ),
+      itemBuilder: (context, index) {
+        final controller = controls[index];
+        return ListTile(
+          title: Hero(tag: "nombre$index", child: Text('Activo $index')),
+          trailing: IconButton(
+            icon: const Icon(Icons.save_outlined),
+            onPressed: controller.guardar,
+          ),
+          subtitle: ReactiveTextFieldTags(
+              formControl: controller.tagsControl,
+              validator: (String tag) {
+                if (tag.isEmpty) return "ingrese algo";
+
+                final splited = tag.split(":");
+
+                if (splited.length == 1) {
+                  return "agregue : para separar la etiqueta";
+                }
+
+                if (splited.length > 2) return "solo se permite un :";
+
+                if (splited[0].isEmpty || splited[1].isEmpty) {
+                  return "agregue texto antes y despues de :";
+                }
+
+                return null;
+              }),
+        );
+      },
     );
   }
 }
