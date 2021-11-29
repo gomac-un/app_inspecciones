872da9be207d6d109742b78297d58ff109c37958
victorcatn <victorcatn@users.noreply.github.com>
2021-11-29 01:07:36 -0500

    bugfix with the useStream hook

--- lib/utils/hooks.dart
+++ lib/utils/hooks.dart
@@ -3,7 +3,7 @@ import 'package:reactive_forms/reactive_forms.dart';
 import 'package:rxdart/rxdart.dart';
 
 T useValueStream<T>(ValueStream<T> subject) =>
-    useStream(subject, initialData: subject.value).requireData;
+    useStream(subject /*, initialData: subject.value*/).data ?? subject.value;
 
 T? useControlValue<T>(AbstractControl<T> control) =>
     useStream(control.valueChanges, initialData: control.value).requireData;
